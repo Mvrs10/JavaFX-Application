79c94509eefd8d7f07b9d104fc362b20deeb5147
Minh Khoi Phan <pmka42@gmail.com>
2024-11-10 06:41:49 -0500

    Adding info fields

--- src/comp228_mkp_lab4_exercise1/JavaFX.java
+++ src/comp228_mkp_lab4_exercise1/JavaFX.java
@@ -30,11 +30,13 @@ public class JavaFX extends Application{
 		// Create the root container - Flow Pane
 		BorderPane rootBP = createRootBorderPane();
 		// Create the top container - HBox
-		HBox topHbox = createHBox();
+		HBox topContainer = createHBox();
+		GridPane leftContainer = createGridPane();
 		// Populate content
-		rootBP.setTop(topHbox);
+		rootBP.setTop(topContainer);
+		rootBP.setLeft(leftContainer);
 		// Create a scene
-		Scene scene = new Scene(rootBP, 800, 600);
+		Scene scene = new Scene(rootBP, 1000, 600);
 		// Set scene
 		primaryStage.setScene(scene);
 		primaryStage.show();
@@ -59,12 +61,53 @@ public class JavaFX extends Application{
 		HBox hbox = new HBox();
 		// Set properties
 		hbox.setPadding(new Insets(20, 0, 20, 0));
-		// Set content
+		// Add contents
 		Label header = new Label("Student Registration Form");
 		header.setFont(Font.font("Times New Roman", FontWeight.BOLD, 30));
 		hbox.getChildren().add(header);	
 		hbox.setAlignment(Pos.BASELINE_CENTER); // Center alignment
 		return hbox;
 	}
-
+	// Left Container function
+	private GridPane createGridPane() {
+		// Instantiate a GridPane
+		GridPane gpane = new GridPane();
+		// Set properties
+		gpane.setPadding(new Insets(5, 10, 5, 10));
+		gpane.setVgap(8);
+		gpane.setHgap(8);
+		// Create labels
+		Label lbName = new Label("Full name:");
+		Label lbAddress = new Label("Address:");
+		Label lbCity = new Label("City:");
+		Label lbProvince = new Label("Province:");
+		Label lbPostalCode = new Label("Postal Code:");
+		Label lbPhoneNumber = new Label("Phone Number:");
+		Label lbEmail = new Label("Email:");
+		// Create TextFields
+		TextField tfName = new TextField();
+		tfName.setPrefWidth(250);
+		TextField tfAddress = new TextField();
+		TextField tfCity = new TextField();
+		TextField tfProvince = new TextField();
+		TextField tfPostalCode = new TextField();
+		TextField tfPhoneNumber = new TextField();
+		TextField tfEmail = new TextField();
+		// Populate contents
+		gpane.add(lbName, 0, 0);
+		gpane.add(tfName, 1, 0);
+		gpane.add(lbAddress, 0, 1);
+		gpane.add(tfAddress, 1, 1);
+		gpane.add(lbCity, 0, 2);
+		gpane.add(tfCity, 1, 2);
+		gpane.add(lbProvince, 0, 3);
+		gpane.add(tfProvince, 1, 3);
+		gpane.add(lbPostalCode, 0, 4);
+		gpane.add(tfPostalCode, 1, 4);
+		gpane.add(lbPhoneNumber, 0, 5);
+		gpane.add(tfPhoneNumber, 1, 5);
+		gpane.add(lbEmail, 0, 6);
+		gpane.add(tfEmail, 1, 6);
+		return gpane;
+	}
 }
