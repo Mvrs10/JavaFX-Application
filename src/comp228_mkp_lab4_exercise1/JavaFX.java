3d638ddc9b2eb3127e5c03aedb190bd21492c8cf
Minh Khoi Phan <pmka42@gmail.com>
2024-11-10 06:09:15 -0500

    Adding title

--- src/comp228_mkp_lab4_exercise1/JavaFX.java
+++ src/comp228_mkp_lab4_exercise1/JavaFX.java
@@ -9,12 +9,17 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Group;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.FontWeight;
 
 public class JavaFX extends Application{
 	// Override the start method of the Application class
@@ -22,8 +27,12 @@ public class JavaFX extends Application{
 	public void start(Stage primaryStage) {
 		
 		primaryStage.setTitle("COMP-228 Lab4 JavaFX - Minh Khoi Phan");
-		// Create a root container - Flow Pane
+		// Create the root container - Flow Pane
 		BorderPane rootBP = createRootBorderPane();
+		// Create the top container - HBox
+		HBox topHbox = createHBox();
+		// Populate content
+		rootBP.setTop(topHbox);
 		// Create a scene
 		Scene scene = new Scene(rootBP, 800, 600);
 		// Set scene
@@ -35,7 +44,7 @@ public class JavaFX extends Application{
 	{
 		launch();
 	}
-	// Container function
+	// Root Container function
 	private BorderPane createRootBorderPane() {
 		// Instantiate a flow pane
 		BorderPane bp = new BorderPane();
@@ -43,6 +52,19 @@ public class JavaFX extends Application{
 		
 		
 		return bp;
+	}
+	// Top Container function
+	private HBox createHBox() {
+		// Instantiate an HBox
+		HBox hbox = new HBox();
+		// Set properties
+		hbox.setPadding(new Insets(20, 0, 20, 0));
+		// Set content
+		Label header = new Label("Student Registration Form");
+		header.setFont(Font.font("Times New Roman", FontWeight.BOLD, 30));
+		hbox.getChildren().add(header);	
+		hbox.setAlignment(Pos.BASELINE_CENTER); // Center alignment
+		return hbox;
 	}
 
 }
